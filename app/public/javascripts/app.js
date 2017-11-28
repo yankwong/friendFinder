820ece71e40d7995e6975415cede97081884cbd6
Yan Kwong <yankwong@Yans-MacBook-Air.local>
2017-11-28 12:11:01 -0800

    updated frontend score logic

--- app/public/javascripts/app.js
+++ app/public/javascripts/app.js
@@ -1,22 +1,38 @@
 var YTK = YTK || {};
 
 YTK.friends = (function($) {
-  var 
+  var TOTALQUESTIONS = 2,
+  getSurveyAns = function(total) {
+    var surveyPage = '.survey-page',
+        results = [];
+
+    if (typeof total === 'undefined' || total <= 0) {
+      total = TOTALQUESTIONS;
+    }
+
+    for (var i=0; i < total; i++) {
+      var $question = $('.q-'+i, surveyPage),
+          answer = parseInt($question.val().trim());
+
+          results.push(answer);
+    }
+    return results;
+  },
+  getTotalScore = function(scoreArr) {
+    return scoreArr.reduce(function(total, num) {
+      return total + parseInt(num);
+    })
+  },
   getFormData = function() {
     var name  = $('.user-name', '#friend-survey').val().trim(),
-        image = $('.user-img', '#friend-survey').val().trim(),
-        q_0   = $('.q-0', '#friend-survey').val().trim(),
         retVal = {},
-        survey = [],
-        score = parseInt(q_0);
-
-    survey.push({q0 : q_0});
+        survey = getSurveyAns(),
+        score = getTotalScore(survey);
 
     retVal = {
-      name  : name,
-      image : image,
-      survey   : survey,
-      score : score
+      name      : name,
+      survey    : survey,
+      score     : score
     };
 
     return retVal;
@@ -30,27 +46,27 @@ YTK.friends = (function($) {
     $submit.on('click', function(e) {
       e.preventDefault();
       var dataObj = getFormData();
-
+      console.log('form result', dataObj);
       if (verifyForm(dataObj)) {
 
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/api/friends", true);
         xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
         xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-              console.log('all good', JSON.stringify(dataObj));
-            }
-            else if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 404) {
-              console.log('all bad');
-            }
+          if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log('all good', JSON.stringify(dataObj));
+          }
+          else if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 404) {
+            console.log('all bad');
+          }
         }
         xhr.send(JSON.stringify(dataObj));
       }
       else {
         alert('Please fill out all the required fields');
+        return false;
       }
-
-    })
+    });
   },
   initPage = function() {
     initSubmitForm();
